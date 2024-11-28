1c78cae11a5f307ed0c361f45cea6045342263cb
messele <messele@tukulllc.com>
2024-11-28 13:15:49 -0500

    scafolded projet

---
+++ src/views/UserProfileView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <main>
+    <h1>Welcome {{ route.params?.name }}</h1>
+  </main>
+</template>
+<style lang=""></style>
